8b214b4b0e048cd0751e647f192ddaef51ca0ce9
Tong Zhang <zhangtong.purdue@gmail.com>
2018-05-07 15:56:33 -0700

    Started to work on templates/homepage

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
+from pprint import pformat
 import os
 
+import requests
 import httplib
 from flask import Flask, render_template, request, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
@@ -8,19 +10,31 @@ app = Flask(__name__)
 app.secret_key = "ABC"
 ONBOARD_KEY=os.environ['ONBOARD_KEY']
 
-print ONBOARD_KEY
 # ONBOARD_URL = ""
 
-conn = httplib.HTTPSConnection("search.onboard-apis.com") 
+# conn = httplib.HTTPSConnection("search.onboard-apis.com") 
 
-headers = { 
-    'accept': "application/json", 
-    'apikey': ONBOARD_KEY, 
-    } 
+# headers = { 
+#     'accept': "application/json", 
+#     'apikey': ONBOARD_KEY, 
+#     } 
 
-conn.request("GET", "/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO", headers=headers) 
+# conn.request("GET", "/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO", headers=headers) 
 
-res = conn.getresponse() 
-data = res.read() 
+# res = conn.getresponse() 
+# data = res.read() 
 
-print(data.decode("utf-8"))
+# print(data.decode("utf-8"))
+
+@app.route("/")
+def homepage():
+    """Show homepage."""
+
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    DebugToolbarExtension(app)
+    app.run(host = '0.0.0.0')
