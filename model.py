2b4ba7ee1d2ed99687b2d5577f5bd4b338064145
Tong Zhang <zhangtong.purdue@gmail.com>
2018-05-07 14:14:19 -0700

    Added repr functions

--- model.py
+++ model.py
@@ -15,25 +15,43 @@ class User(db.Model):
     password = db.Column(db.String(64), nullable=True)
     # zipcode = db.Column(db.String(15), nullable=True)
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<User user_id={} email={}>".format(self.user_id, self.email)
+
 class Favorite(db.Model):
     """properities that users marked as favorite."""
 
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "favorites"
+
+    id = db.Column(db.Integer, autoincrement=True,  primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     property_id = db.Column(db.Integer, db.ForeignKey('properties.property_id'))
 
-    __tablename__ = "favorites"
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Favorite id={} user_id={} property_id={}>".format(self.id, self.user_id, self.property_id)
+
+    
 
 class Search(db.Model):
     """User searches for properties"""
 
     __tablename__ = "searches"
 
-    search_id = db.Column(db.Ingeter, autoincrement=True, primary_key=True)
+    search_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     address = db.Column(db.String(250), nullable=True)
     zipcode = db.Column(db.Integer, nullable=True)
     no_of_room = db.Column(db.Integer, nullable=False)
     no_of_bath = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Search search_id={} user_id={} address={} zipcode={} no_of_room={} no_of_bath={}>".format(self.search_id, self.user_id, self.address, self.zipcode, self.no_of_room, self.no_of_bath)
 
 
 class Property(db.Model):
@@ -42,12 +60,15 @@ class Property(db.Model):
     __tablename__ = "properties"
 
     property_id = db.Column(db.Integer, primary_key=True)
-    zipcode = db.Column(db.Integer, db.ForeignKey('searches.zipcode'))
-    proterty_address = db.Column(db.Integer, db.ForeignKey('searches.address'))
+    # search_id = db.Column(db.Integer, db.ForeignKey('searches.search_id')) #no need to referenced here
     latitude = db.Column(db.Float, nullable=True)
     longitude = db.Column(db.Float, nullable=True)
-    zillow_url = db.Column(db.String(200), nullable=True) #Needs to envolves in Zillow API
+    zillow_url = db.Column(db.String(200), nullable=True) #Needs to involve in Zillow API
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Property property_id={} latitude={} longitude={}>".format(self.property_id, self.latitude, self.longitude)
 
 class Sale(db.Model):
     """Sale Details."""
@@ -57,10 +78,15 @@ class Sale(db.Model):
 
     sale_id = db.Column(db.Integer, primary_key=True)
     property_id = db.Column(db.Integer, db.ForeignKey('properties.property_id'))
-    sale_trans_date = db.Column(db.Datetime, nullable=True)
+    sale_trans_date = db.Column(db.DateTime, nullable=True)
     sale_amount = db.Column(db.Float, nullable=False)
     no_of_room = db.Column(db.Integer, nullable=False)
     no_of_bath = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Sale sale_id={} property_id={} sale_trans_date={} sale_amount={}>".format(self.sale_id, self.property_id, self.sale_trans_date, self.sale_amount)
 
 
 # Helper functions
@@ -77,6 +103,8 @@ if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    from server import app
+    # from server import app
+    from flask import Flask
+    app = Flask(__name__)
     connect_to_db(app)
     print "Connected to DB."
